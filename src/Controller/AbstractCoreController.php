56a990edf2abe02e9ac4a829b29ceaafa06067e3
Martin <hello@northrook.com>
2024-02-22 15:03:05 +0000

    mend

--- src/Controller/AbstractCoreController.php
+++ src/Controller/AbstractCoreController.php
@@ -3,47 +3,64 @@
 namespace Northrook\Symfony\Core\Controller;
 
 use Northrook\Symfony\Core\Services\EnvironmentService;
+use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
-use Symfony\Component\Routing\RouterInterface;
 use Symfony\Contracts\Service\Attribute\Required;
-use Twig\Environment;
+use Symfony\Contracts\Service\Attribute\SubscribedService;
 
 abstract class AbstractCoreController extends AbstractController
 {
-	protected ContainerInterface $container;
+	protected ContainerInterface  $container;
 	protected ?EnvironmentService $env;
 
-
+	/** Runs on container initialization.
+	 *
+	 * * Modified from the Symfony AbstractController
+	 * * Initializes additional services
+	 */
 	#[Required]
 	public function setContainer( ContainerInterface $container ) : ?ContainerInterface {
 		$previous = $this->container ?? null;
 		$this->container = $container;
 
-		$this->env = $container->get( 'core.environment_service');
+		if ( $container->has( 'core.environment_service' ) ) {
+			$this->env = $container->get( 'core.environment_service' );
+		}
 
 		return $previous;
 	}
 
-	public static function getSubscribedServices(): array
-	{
-		return [
-			'router' => '?'.RouterInterface::class,
-			'request_stack' => '?'.RequestStack::class,
-			'http_kernel' => '?'.HttpKernelInterface::class,
-			'serializer' => '?'.SerializerInterface::class,
-			'security.authorization_checker' => '?'.AuthorizationCheckerInterface::class,
-			'twig' => '?'.Environment::class,
-			'form.factory' => '?'.FormFactoryInterface::class,
-			'security.token_storage' => '?'.TokenStorageInterface::class,
-			'security.csrf.token_manager' => '?'.CsrfTokenManagerInterface::class,
-			'parameter_bag' => '?'.ContainerBagInterface::class,
-			'web_link.http_header_serializer' => '?'.HttpHeaderSerializer::class,
-			'core.environment_service' => '?'.EnvironmentService::class,
-		];
+	/** Get subscribed services
+	 *
+	 * Subscribes to additional services:
+	 * * core.environment_service
+	 *
+	 *
+	 * @return string[]|SubscribedService[]
+	 *  */
+	public static function getSubscribedServices() : array {
+		return array_merge(
+			parent::getSubscribedServices(),
+			[
+				'core.environment_service' => '?' . EnvironmentService::class,
+			],
+		);
+	}
+
+	protected function latte(
+		string         $template,
+		object | array $parameters = [],
+	) : string {
+		return '';
+	}
+
+	protected function renderLatte(
+		string         $template,
+		object | array $parameters = [],
+	) : string {
+		return '';
 	}
 
 
--- src/SymfonyCoreBundle.php
+++ src/SymfonyCoreBundle.php
@@ -4,7 +4,7 @@ namespace Northrook\Symfony\Core;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class SymfonyCoreBundle extends Bundle
+final class SymfonyCoreBundle extends Bundle
 {
 	public function getPath() : string {
 		return dirname( __DIR__ );
