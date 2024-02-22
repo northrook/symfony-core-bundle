84f27f5fa15bb39cd42e6b1e8e6b54f867d9b3e9
Martin <hello@northrook.com>
2024-02-22 10:48:06 +0000

    mend

--- config/services.php
+++ config/services.php
@@ -4,7 +4,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Northrook\Symfony\Core\Services\EnvironmentService;
 
-function fromRoot( ?string $set = null ) : string {
+function fromRoot( string $set = '' ) : string {
 	return '%kernel.project_dir%' . DIRECTORY_SEPARATOR . trim(
 			str_replace( [ '\\', '/' ], DIRECTORY_SEPARATOR, $set ), DIRECTORY_SEPARATOR,
 		) . DIRECTORY_SEPARATOR;
@@ -13,19 +13,23 @@ function fromRoot( ?string $set = null ) : string {
 return static function ( ContainerConfigurator $container ) : void {
 
 	$container->parameters()
-			  ->set( 'env', '%kernel.environment%' )
-			  ->set( 'dir.root', fromRoot() )
-			  ->set( 'dir.public', fromRoot( "/public" ) )
-			  ->set( 'dir.templates', fromRoot( "/templates" ) )
-			  ->set( 'dir.cache', fromRoot( "/var/cache" ) )
-			  ->set( 'dir.cache.latte', fromRoot( "/var/cache/latte" ) )
-			  ->set( 'ttl.cache', 86400 )
+	          ->set( 'env', '%kernel.environment%' )
+	          ->set( 'dir.root', fromRoot() )
+	          ->set( 'dir.public', fromRoot( "/public" ) )
+	          ->set( 'dir.templates', fromRoot( "/templates" ) )
+	          ->set( 'dir.cache', fromRoot( "/var/cache" ) )
+	          ->set( 'dir.cache.latte', fromRoot( "/var/cache/latte" ) )
+	          ->set( 'ttl.cache', 86400 )
 	;
 
 	$container->services()
-			  ->set( 'core.environment_service', EnvironmentService::class )
-			  ->public()
-			  ->alias( EnvironmentService::class, 'core.environment_service' )
+	          ->set( 'core.environment_service', EnvironmentService::class )
+	          ->args( [
+		                  service( 'parameter_bag' ),
+		                  service( 'logger' )->nullOnInvalid(),
+	                  ] )
+	          ->public()
+	          ->alias( EnvironmentService::class, 'core.environment_service' )
 	;
 
 };
--- src/DependencyInjection/SymfonyCoreExtension.php
+++ src/DependencyInjection/SymfonyCoreExtension.php
@@ -14,7 +14,7 @@ final class SymfonyCoreExtension extends Extension
 	/**
 	 * @throws Exception
 	 */
-	public function load( array $configs, ContainerBuilder $container ) {
+	public function load( array $configs, ContainerBuilder $container ) : void {
 		$bundle = dirname( __DIR__, 2 );
 		$config = new PhpFileLoader( $container, new FileLocator( $bundle . '/config' ) );
 		$config->load( 'services.php' );
