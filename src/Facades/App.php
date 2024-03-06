d3331471de5dced6b97064d55bea8edfa2997687
Martin <hello@northrook.com>
2024-03-06 06:43:38 +0000

    mend

--- src/Facades/App.php
+++ src/Facades/App.php
@@ -4,31 +4,27 @@ namespace Northrook\Symfony\Core\Facades;
 
 use JetBrains\PhpStorm\ExpectedValues;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use UnitEnum;
 
 final class App extends AbstractFacade
 {
 
-	/**
-	 * @param  string  $get  {@see ParameterBagInterface::get}
-	 * @return string|int|bool|array|float|UnitEnum|null
-	 */
-	public static function getParameter(
-		string $get,
-	) : string | int | bool | array | null | float | UnitEnum {
-		return self::parameterBag()->get( $get );
+	private static function kernel() : HttpKernelInterface {
+		return self::getContainerService( 'kernel' );
 	}
 
 	public static function env(
 		#[ExpectedValues( [ 'dev', 'prod', 'debug' ] )]
 		string $is,
 	) : bool {
-		return match ( $is ) {
-			'dev'   => self::getParameter( 'kernel.environment' ) == 'dev',
-			'prod'  => self::getParameter( 'kernel.environment' ) == 'prod',
-			'debug' => self::getParameter( 'kernel.debug' ) === true,
-			default => false,
-		};
+		return false;
+//		return match ( $is ) {
+//			'dev'   => self::getParameter( 'kernel.environment' ) == 'dev',
+//			'prod'  => self::getParameter( 'kernel.environment' ) == 'prod',
+//			'debug' => self::getParameter( 'kernel.debug' ) === true,
+//			default => false,
+//		};
 	}
 
 }
