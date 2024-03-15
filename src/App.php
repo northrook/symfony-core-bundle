bcf9b98b9f15f2cce37f968e4cec0189d0a57bc5
Martin <hello@northrook.com>
2024-03-15 08:07:12 +0000

    mend

--- src/App.php
+++ src/App.php
@@ -6,36 +6,23 @@ use JetBrains\PhpStorm\ExpectedValues;
 
 final class App extends SymfonyCoreFacade
 {
+
     /**
      * @param string  $is
      *
      * @return bool
      * @todo 'public' functionality reads the Site Settings Entity, but this is not yet implemented
-     *
      */
     public static function env(
-        #[ExpectedValues( [ 'dev', 'prod', 'debug', 'public', null ] )]
-        ?string $is = null,
-    ) : bool | object {
-
-        $environment = [
-            'dev'   => App::getKernel()->getEnvironment() == 'dev',
-            'prod'  => App::getKernel()->getEnvironment() == 'prod',
-            'debug' => App::getKernel()->isDebug(),
-            // Check if global is public, then check if current route is public
-            // 'public' => Settings::site()->isPublic,
-        ];
-
-        return $is ? $environment[ $is ] ?? false : (object) $environment;
-
-//        return $environment[ $is ] ?? (object) $environment;
-
-//        return match ( $is ) {
-//            'dev'   => App::getKernel()->getEnvironment() == 'dev',
-//            'prod'  => App::getKernel()->getEnvironment() == 'prod',
-//            'debug' => App::getKernel()->isDebug(),
-////            'public' => Settings::site()->isPublic, // Check if global is public, then check if current route is public
-//            default => false,
-//        };
+        #[ExpectedValues( [ 'dev', 'prod', 'debug', 'public' ] )]
+        string $is,
+    ) : bool {
+        return match ( $is ) {
+            'dev'   => self::getKernel()->getEnvironment() == 'dev',
+            'prod'  => self::getKernel()->getEnvironment() == 'prod',
+            'debug' => self::getKernel()->isDebug(),
+//            'public' => Settings::site()->isPublic, // Check if global is public, then check if current route is public
+            default => false,
+        };
     }
 }
--- src/AssetManager/AssetManager.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Northrook\Symfony\Core\AssetManager;
-
-class AssetManager
-{
-
-}
