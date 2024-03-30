6273a058018e4580f4f3fd425fd2bae261257b5a
Martin <hello@northrook.com>
2024-03-30 11:50:13 +0000

    mend

--- config/routes.php
+++ config/routes.php
@@ -1,13 +1,12 @@
 <?php
 declare( strict_types = 1 );
 
-use Northrook\Symfony\Core\File;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function ( RoutingConfigurator $routes ) : void {
     $routes->import(
         [
-            'path'      => File::parameterDirname( '../../src/Controller' ),
+            'path'      => dirname( __DIR__, 2 ) . '/src/Controller',
             'namespace' => 'App\Controller',
         ],
         'attribute',
