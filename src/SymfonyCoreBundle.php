03a568ee1305ff99de911946ccb26b75af021b5e
Martin <hello@northrook.com>
2024-03-30 13:15:39 +0000

    mend

--- src/SymfonyCoreBundle.php
+++ src/SymfonyCoreBundle.php
@@ -4,10 +4,15 @@ declare( strict_types = 1 );
 
 namespace Northrook\Symfony\Core;
 
+use Northrook\Symfony\Core\Controller\CoreApiController;
 use Northrook\Symfony\Core\DependencyInjection\ControllerRegistrationPass;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 
 /**
  * @version 1.0 ☑️
@@ -18,6 +23,27 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
  */
 final class SymfonyCoreBundle extends AbstractBundle
 {
+
+    use MicroKernelTrait {
+        loadRoutes as protected loadRoutesKernel;
+    }
+
+    public function loadRoutes( LoaderInterface $loader ) : RouteCollection {
+        // Loading existing routes
+        $collection = $this->loadRoutesKernel( $loader );
+
+        // you condition with some param, example form $_ENV or $this->getContainer()->getParameter('some_param')
+
+        $collection->add(
+            'dynamic-route', new Route(
+            '/docs/test', [
+            '_controller' => [ CoreApiController::class, 'favicon' ],
+        ],
+        ),
+        );
+
+        return $collection;
+    }
 
     public function loadExtension(
         array                 $config,
