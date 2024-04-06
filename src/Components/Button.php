3a1ac3e869a8fe065b1c4350d775e89e4f1b56db
Martin <hello@northrook.com>
2024-04-06 08:21:07 +0100

    mend

--- src/Components/Button.php
+++ src/Components/Button.php
@@ -4,6 +4,32 @@ namespace Northrook\Symfony\Core\Components;
 
 use Northrook\Symfony\Core\Latte\Component;
 
+/**
+ * # Button
+ *
+ * * Renders `<button type="auto" ... >[content]</button>`
+ *
+ * Attributes:
+ * * `name` - The name of the button
+ * * `value` - The value of the button
+ * * `type` - The type of button. Defaults to `button` unless set
+ * * `class` - The class of the button
+ * * `content` - innerHTML of the button
+ * * `icon` - Renders an icon as innerHTML
+ * * `disabled` - Whether the button is disabled
+ *
+ * InnerHTML templating:
+ * * `{icon}` - Renders an icon if set
+ * * `{content}` - Renders innerHTML if set
+ *
+ * ```
+ * // example.latte
+ * <button icon="close:ui" content="{icon} Close something"/>
+ * // rendered html
+ * <button type="button" class="icon"><i><svg/></i>Close something</button>
+ * ```
+ *
+ */
 class Button extends Component
 {
 
--- src/Latte/LatteComponentPreprocessor.php
+++ src/Latte/LatteComponentPreprocessor.php
@@ -6,22 +6,25 @@ use Northrook\Support\Regex;
 use Northrook\Symfony\Core\Components\Button;
 use Northrook\Symfony\Latte\Preprocessor\Preprocessor;
 
-// NOTE : This is a "source" file, use it to template a new core component
-//
-
+/**
+ * # Field Component Preprocessor
+ *
+ * * Matches `<field:component ... >` tags in the {@see $content} string.
+ * * Processes and replaces the matched components in the {@see $content} string.
+ */
 final class LatteComponentPreprocessor extends Preprocessor
 {
 
+    private const COMPONENTS = [
+        'button' => Button::class,
+    ];
+
     /**
-     * @var Component[]
+     * A list of all matched components in the {@see $content} string.
      */
     private array $components = [];
 
-    public function __construct() {
-        $this->components = [
-            Button::class,
-        ];
-    }
+    public function __construct() {}
 
     public function addComponent(
         string  ...$component
@@ -50,14 +53,14 @@ final class LatteComponentPreprocessor extends Preprocessor
     }
 
     public function process() : self {
-        $this->prepareContent();
+        $this->prepareContent( true );
         $this->matchComponents();
-        $this->processButtons();
-        $this->processIcons();
+        // $this->processButtons();
+        // $this->processIcons();
 
-        foreach ( $this->components as $component ) {
-            $this->updateContent( $component->templateString, $component );
-        }
+        // foreach ( $this->components as $component ) {
+        //     $this->updateContent( $component->templateString, $component );
+        // }
 
         return $this;
     }
@@ -77,181 +80,17 @@ final class LatteComponentPreprocessor extends Preprocessor
             return;
         }
 
-        dd( $components );
+        // dd( $components );
 
-//         foreach ( $components as $match ) {
-//
-//             $element = Component::element( $match, $this->logger, $this->stopwatch );
-//
-//             // Looks forwards to find innerHTML
-//             if (
-//                 false === str_ends_with( $match->string, '/>' )
-//                 &&
-        /*                false !== preg_match( "/<\s*?\/\s*?$match->component\s*?>/ms", $this->content, $closingTag )*/
-//             ) {
-//
-//                 $closingTag = $closingTag[ 0 ] ?? false;
-//
-//                 if ( $closingTag === false ) {
-//                     $this->logger->error(
-//                         'Closing tag expected, but not found for {match}',
-//                         [ 'match' => $match->component ],
-//                     );
-//                 }
-//
-//                 $component = strpos( $this->content, $match->string );
-//                 $closing   = strpos( $this->content, $closingTag );
-//
-//                 $outerHTML = substr(
-//                     string : $this->content,
-//                     offset : $component,
-//                     length : $closing - $component + strlen( $closingTag ),
-//                 );
-//
-//                 $innerHTML = substr(
-//                     $outerHTML,
-//                     strlen( $match->string ),
-//                     (
-//                         strlen( $outerHTML )
-//                         - strlen( $match->string )
-//                         - strlen( $closingTag )
-//                     ),
-//                 );
-//
-//                 $innerHTML     = trim( $innerHTML );
-//                 $this->content = str_ireplace( $outerHTML, $match->string, $this->content );
-//
-//                 $element->innerHTML = $innerHTML;
-// //				$node[ 'innerHTML' ] = $innerHTML;
-//             }
-//             $this->components[] = $element;
-//
-// //			$this->components[] = $node;
-//         }
-//		dd( $this->components );
-    }
+        foreach ( $components as $match ) {
 
-    private function processButtons() : void {
+            $element            = $match;
+            $this->components[] = $element;
 
-        if ( !str_contains( $this->content, '<button ' ) ) {
-            return;
+//			$this->components[] = $node;
         }
-
-        $this->content = preg_replace_callback(
-            pattern  : "/<button(.*?)>/ms",
-            callback : static function ( $element ) : string {
-
-                $button = new Element(
-                    tag   : 'button',
-                    close : str_ends_with( $element[ 0 ], '/>' ),
-                );
-
-                $button->extractAttributes( trim( $element[ 1 ], ' /' ) );
-
-                if ( $button->has( 'icon' ) ) {
-                    $button->class->add( 'icon' );
-                    $button->content[] = Asset::svg( $button->get( 'icon' ) );
-//					dd( $button);
-                    $button->remove( 'icon' );
-                }
-
-                if ( $button->has( 'text' ) ) {
-                    $button->content[] = $button->get( 'text' );
-                    $button->remove( 'text' );
-                }
-
-                return $button->print();
-            },
-            subject  : $this->content,
-        );
+        dd( $this->components );
     }
 
-    private function processIcons() : void {
-
-        if ( !str_contains( $this->content, '<icon ' ) ) {
-            return;
-        }
-
-        $this->content = preg_replace_callback(
-            pattern  : " /<icon(.*?)>/ms",
-            callback : static function ( $element ) : string {
-
-                $icon = Icon::asset(
-                    $attributes[ 'get' ] ?? null,
-                    $attributes[ 'stroke' ] ?? null,
-                )->extractAttributes( trim( $element[ 1 ], ' /' ) );
-
-
-//				$attributes = trim( $element[ 1 ], ' /' );
-
-//				$attributes = LatteComponentPreprocessor::extractAttributes( "<icon $attributes > " );
-//
-//				$get = $attributes[ 'get' ] ?? 'angry';
-//				unset( $attributes[ 'get' ] );
-//
-//				if ( isset( $attributes[ 'class' ] ) ) {
-//					$attributes[ 'class' ] = 'icon ' . implode( ' ', (array) $attributes[ 'class' ] );
-//				}
-//				else {
-//					$attributes[ 'class' ] = 'icon';
-//				}
-//
-//				$icon = new Element(
-//					tag        : 'i',
-//					attributes : $attributes,
-//					content    : Asset::icon( $get, raw : true ),
-//				);
-
-//				dd( $icon );
-                return $icon->print();
-            },
-            subject  : $this->content,
-        );
-    }
-
-    private function images() : void {
-
-        if ( !str_contains( $this->content, '<image ' ) ) {
-            return;
-        }
-
-        $this->content = preg_replace_callback(
-            pattern  : "/<image(.*?)>/ms",
-            callback : static function ( $element ) : string {
-
-//				$attributes = trim( $element[ 1 ], ' /' );
-//				$attributes = static::extractAttributes( $attributes );
-
-//				$isExternal = Str::isUrl( $attributes[ 'src' ] ?? '' );
-
-                $image = new Image();
-                $image->extractAttributes( trim( $element[ 1 ], ' /' ) );
-
-                // dd( $image, $image->figure() );
-
-                return $image->print();
-
-                // $get = $attributes['get'] ?? 'angry';
-                // unset( $attributes['get'] );
-
-                // if ( isset( $attributes['class'] ) ) {
-                // 	$attributes['class'] = 'icon ' . implode( ' ', (array) $attributes['class'] );
-                // } else {
-                // 	$attributes['class'] = 'icon';
-                // }
-
-                // $icon = new Element( 'i', $attributes, Get::icon( $get, raw: true ) );
-
-                // return Str::squish( $icon );
-            },
-            subject  : $this->content,
-        );
-    }
-
-    /**
-     * @todo Avatar Component
-     * Has to grab the avatar user entity
-     *
-     */
 
 }
