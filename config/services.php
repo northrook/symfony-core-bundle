137f85ce6af9c77be9325bc5ba1108302de37fa0
Martin <hello@northrook.com>
2024-05-13 09:05:23 +0100

    mend

--- config/services.php
+++ config/services.php
@@ -2,26 +2,7 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use Northrook\Favicon\FaviconBundle;
-use Northrook\Symfony\Components\LatteComponentPreprocessor;
-use Northrook\Symfony\Core\Controller\AdminController;
-use Northrook\Symfony\Core\Controller\ApiController;
-use Northrook\Symfony\Core\Controller\PublicController;
-use Northrook\Symfony\Core\Controller\SecurityController;
-use Northrook\Symfony\Core\DependencyInjection\CoreDependencies;
-use Northrook\Symfony\Core\EventListener\ExceptionListener;
-use Northrook\Symfony\Core\EventSubscriber\LogAggregationSubscriber;
-use Northrook\Symfony\Core\EventSubscriber\ResponseEventSubscriber;
-use Northrook\Symfony\Core\File;
-use Northrook\Symfony\Core\Services\CurrentRequestService;
-use Northrook\Symfony\Core\Services\DocumentService;
-use Northrook\Symfony\Core\Services\FormService;
-use Northrook\Symfony\Core\Services\MailerService;
-use Northrook\Symfony\Core\Services\PathfinderService;
-use Northrook\Symfony\Core\Services\SettingsManagementService;
-use Northrook\Symfony\Core\Services\StylesheetGenerationService;
-use Symfony\Component\DependencyInjection\ServiceLocator;
-use Symfony\Component\HttpKernel\Profiler\Profiler;
+use Northrook\Favicon\FaviconBundle;use Northrook\Symfony\Components\LatteComponentPreprocessor;use Northrook\Symfony\Core\Controller\AdminController;use Northrook\Symfony\Core\Controller\ApiController;use Northrook\Symfony\Core\Controller\PublicController;use Northrook\Symfony\Core\Controller\SecurityController;use Northrook\Symfony\Core\DependencyInjection\CoreDependencies;use Northrook\Symfony\Core\EventListener\ExceptionListener;use Northrook\Symfony\Core\EventSubscriber\LogAggregationSubscriber;use Northrook\Symfony\Core\EventSubscriber\ResponseEventSubscriber;use Northrook\Symfony\Core\File;use Northrook\Symfony\Core\Services\CurrentRequestService;use Northrook\Symfony\Core\Services\DocumentService;use Northrook\Symfony\Core\Services\FormService;use Northrook\Symfony\Core\Services\MailerService;use Northrook\Symfony\Core\Services\PathfinderService;use Northrook\Symfony\Core\Services\SettingsManagementService;use Northrook\Symfony\Core\Services\StylesheetGenerationService;use Psr\Log\LoggerInterface;use Symfony\Component\DependencyInjection\ServiceLocator;use Symfony\Component\HttpKernel\HttpKernelInterface;use Symfony\Component\HttpKernel\Profiler\Profiler;use Symfony\Component\Routing\RouterInterface;use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;use Symfony\Component\Serializer\SerializerInterface;use Symfony\Component\Stopwatch\Stopwatch;
 
 return static function ( ContainerConfigurator $container ) : void {
 
@@ -49,23 +30,19 @@ return static function ( ContainerConfigurator $container ) : void {
     //--------------------------------------------------------------------
 
     $services->set( 'core.service.locator', ServiceLocator::class )
+
     ->tag( 'container.service_locator' )
-    ->args([
-                     service( 'router' ),
-                     service( 'http_kernel' ),
-                     service( 'parameter_bag' ),
-                     service( 'core.service.request' ),
-                     service( 'serializer' ),
-                     service( 'security.authorization_checker' ),
-                     service( 'security.token_storage' ),
-                     service( 'security.csrf.token_manager' ),
-                     service( 'latte.environment' ),
-                     service( 'core.service.document' ),
-                     service( 'core.service.stylesheet' ),
-                     service( 'core.service.mailer' ),
-                     service( 'logger' )->nullOnInvalid(),
-                     service( 'debug.stopwatch' )->nullOnInvalid(),
-])
+    ->args([[
+                     RouterInterface::class =>service( 'router' ),
+                     HttpKernelInterface::class =>service( 'http_kernel' ),
+                     CurrentRequestService::class => service( 'core.service.request' ),
+                     SerializerInterface::class => service( 'serializer' ),
+                     AuthorizationCheckerInterface::class => service( 'security.authorization_checker' ),
+                     TokenStorageInterface::class => service( 'security.token_storage' ),
+                     CsrfTokenManagerInterface::class => service( 'security.csrf.token_manager' ),
+                     LoggerInterface::class => service( 'logger' )->nullOnInvalid(),
+                     Stopwatch::class => service( 'debug.stopwatch' )->nullOnInvalid(),
+]])
     ->public();
 
     //
--- src/DependencyInjection/Facade.php
+++ src/DependencyInjection/Facade.php
@@ -2,7 +2,40 @@
 
 namespace Northrook\Symfony\Core\DependencyInjection;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;use Symfony\Component\DependencyInjection\ServiceLocator;
+
 abstract class Facade {
+
+
+    private static ContainerInterface $instance;
+    private static ?ServiceLocator $serviceLocator = null;
+
+    public static function container( ContainerInterface $container ) : void {
+        Facade::$instance = $container;
+        Facade::$serviceLocator = $container->get(
+            id: 'core.service.locator',
+             invalidBehavior: ContainerInterface::NULL_ON_INVALID_REFERENCE,
+             );
+    }
+
+
+    /**
+ * @param string | class-string $serviceId
+*
+* @return mixed
+ */
+    private static function get( string $serviceId ) : mixed {
+
+        if (Facade::$instance->has($serviceId) ) {
+            return Facade::$instance->get($serviceId);
+        }
+
+        if ( Facade::$serviceLocator?->has($serviceId) ) {
+            return Facade::$serviceLocator->get($serviceId);
+        }
+
+        return null;
+    }
 
     /**
      * Get the service identifier.
@@ -18,7 +51,7 @@ abstract class Facade {
      */
     public static function instance()
     {
-        return Container::get(static::getServiceIdentifier());
+        return Facade::get(static::getServiceIdentifier());
     }
 
     /**
