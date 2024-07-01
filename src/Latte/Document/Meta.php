417a94764f875362f7a63606c7fa50deae488c97
Martin <hello@northrook.com>
2024-07-01 08:39:37 +0100

    Refactor started.

--- src/Latte/Document/Meta.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Northrook\Symfony\Core\Latte\Document;
-
-class Meta
-{
-
-}
--- src/SymfonyCoreBundle.php
+++ src/SymfonyCoreBundle.php
@@ -4,9 +4,6 @@ declare( strict_types = 1 );
 
 namespace Northrook\Symfony\Core;
 
-use Northrook\Support\File;
-use Northrook\Symfony\Core\Support\Console;
-use Northrook\Types\Path;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
@@ -40,69 +37,36 @@ final class SymfonyCoreBundle extends AbstractBundle
         ],
     ];
 
-    private readonly string $projectDir;
-
     public function loadExtension(
         array                 $config,
         ContainerConfigurator $container,
         ContainerBuilder      $builder,
     ) : void {
-        $this->projectDir ??= $builder->getParameterBag()->get( 'kernel.project_dir' );
-
-        $container->import( '../config/cache.php' );
-        $container->import( '../config/services.php' );
-        $container->import( '../config/facades.php' );
-        $container->import( '../config/controllers.php' );
+        echo __METHOD__ . PHP_EOL;
+        // $this->projectDir ??= $builder->getParameterBag()->get( 'kernel.project_dir' );
+        //
+        // $container->import( '../config/cache.php' );
+        // $container->import( '../config/services.php' );
+        // $container->import( '../config/facades.php' );
+        // $container->import( '../config/controllers.php' );
 
         // Autoconfigure Notes
         // Look for .yaml files in config folder, remove them if adding .php version and vice versa
         // TODO : Autoconfigure Security
-        $this->autoconfigureRoutes();
+        // $this->autoconfigureRoutes();
     }
 
     public function boot() : void {
         parent::boot();
-        new Env(
-            $this->container->getParameter( 'kernel.environment' ),
-            $this->container->getParameter( 'kernel.debug' ),
-        );
-        DependencyInjection\Facade\Container::set( $this->container );
+        echo __METHOD__ . PHP_EOL;
+        // new Env(
+        //     $this->container->getParameter( 'kernel.environment' ),
+        //     $this->container->getParameter( 'kernel.debug' ),
+        // );
+        // DependencyInjection\Facade\Container::set( $this->container );
     }
 
     public function getPath() : string {
         return dirname( __DIR__ );
-    }
-
-    private function autoconfigureRoutes() : void {
-
-        $apiController = new Path( $this->projectDir . '/config/routes/core.yaml' );
-        $coreConfig    = [];
-
-        if ( $apiController->exists ) {
-            if ( 'cli' === PHP_SAPI ) {
-                echo Console::info( 'northrook.core.api', 'Config exists: ' . $apiController->value );
-            }
-            return;
-        }
-
-        foreach ( SymfonyCoreBundle::ROUTES as $key => $value ) {
-            $coreConfig[] = "$key:\n    resource: '{$value['resource']}'\n    prefix: {$value['prefix']}\n";
-        }
-
-        $status = File::save(
-            $this->projectDir . '/config/routes/core.yaml',
-            implode( PHP_EOL, $coreConfig ),
-        );
-
-        if ( 'cli' !== PHP_SAPI ) {
-            return;
-        }
-
-        if ( !$status ) {
-            echo Console::error( 'northrook.core.api:', 'Config file not created: ' . $apiController->value );
-        }
-        else {
-            echo Console::OK( 'northrook.core.api:', 'Config created: ' . $apiController->value );
-        }
     }
 }
