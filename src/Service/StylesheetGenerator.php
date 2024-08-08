821a56a20df7150927960a6cf88b4f7379ebda14
Martin <hello@northrook.com>
2024-08-08 06:53:18 +0100

    mend

--- src/Service/StylesheetGenerator.php
+++ src/Service/StylesheetGenerator.php
@@ -59,7 +59,7 @@ final class StylesheetGenerator
         }
 
         $admin = new Stylesheet(
-            $this->pathfinder->get( 'dir.var.stylesheet' ),
+            $this->pathfinder->get( 'path.admin.stylesheet' ),
             [
                 $this->designSystem->admin()->colorPalette->generateStyles(),
                 $this->pathfinder->get( 'dir.core.assets/admin/styles' ),
