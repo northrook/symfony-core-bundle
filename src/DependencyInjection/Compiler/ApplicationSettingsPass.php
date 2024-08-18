0abf78c757ffff4a41c84a6d25831f877c13d57f
Martin <hello@northrook.com>
2024-08-18 12:48:39 +0100

    mend

--- src/DependencyInjection/Compiler/ApplicationSettingsPass.php
+++ src/DependencyInjection/Compiler/ApplicationSettingsPass.php
@@ -18,10 +18,65 @@ final readonly class ApplicationSettingsPass implements CompilerPassInterface
 
         // Assign the path parameters to the Pathfinder service
         $container->getDefinition( Settings::class )
-                  ->replaceArgument(
-                      0,
-                      $this->getPathEntries( $container->getParameterBag() ),
-                  );
+                  ->replaceArgument( 0, $this->getCoreSettings( $container->getParameterBag() ) );
+    }
+
+    private function getCoreSettings( ParameterBagInterface $parameters ) : array {
+
+        $dirRoot  = $parameters->get( 'kernel.project_dir' );
+        $dirCache = $parameters->get( 'kernel.cache_dir' );
+
+        $settings = [
+            'app.env'   => $parameters->get( 'kernel.environment' ),
+            'app.debug' => $parameters->get( 'kernel.debug' ),
+            'app.mode'  => $parameters->get( 'kernel.runtime_mode' ),
+        ];
+
+        $paths = [
+            'dir.root'           => $dirRoot,
+            'dir.src'            => $dirRoot . '/src',
+            'dir.public'         => $dirRoot . '/public',
+            'dir.public.assets'  => $dirRoot . '/public/assets',
+            'dir.var'            => $dirRoot . '/var',
+            'dir.cache'          => $dirCache,
+            'dir.cache.latte'    => $dirCache . '/latte',
+            'dir.storage'        => $dirRoot . '/storage',
+            'dir.build'          => $parameters->get( 'kernel.build_dir' ),
+            'dir.logs'           => $parameters->get( 'kernel.logs_dir' ),
+            'dir.manifest'       => $dirRoot . '/var/manifest',
+            'dir.config'         => $dirRoot . '/config',
+            'dir.assets'         => $dirRoot . '/assets',
+            'dir.templates'      => $dirRoot . '/templates',
+            'dir.core.assets'    => \dirname( __DIR__, 2 ) . '/assets',
+            'dir.core.templates' => \dirname( __DIR__, 2 ) . '/templates',
+        ];
+
+        foreach ( $paths as $name => $path ) {
+            $settings[ $name ] = normalizePath( $path );
+        }
+
+        $settings += [
+            'site.public'            => false,
+            'site.name'              => 'Symfony',
+            'site.url'               => $parameters->get( 'kernel.url' ),
+            'site.locale'            => $parameters->get( 'kernel.default_locale' ),
+            'site.locales_available' => $parameters->get( 'kernel.enabled_locales' ),
+            'site.charset'           => \strtolower( $parameters->get( 'kernel.charset' ) ),
+        ];
+
+        $settings += [
+            'admin.url' => $parameters->get( 'kernel.url' ) . '/admin',
+        ];
+
+        $settings += [
+            'mailer.dsn'  => $_ENV[ 'HOME_URL' ] ?? null,
+            'mailer.from' => $_ENV[ 'MAILER_DSN' ] ?? null,
+            'mailer.name' => $_ENV[ 'MAILER_FROM' ] ?? null,
+            'mailer.lang' => $_ENV[ 'MAILER_NAME' ] ?? null,
+        ];
+        echo \count( $settings ) . ' generated';
+
+        return $settings;
     }
 
     /**
@@ -31,14 +86,15 @@ final readonly class ApplicationSettingsPass implements CompilerPassInterface
      * - Only keys containing `dir` or `path` will be considered
      * - Only values starting with the {@see projectDir} are used
      *
-     * @param ParameterBagInterface  $parameterBag
+     * @param array  $parameters
      *
      * @return array
      */
-    private function getPathEntries( ParameterBagInterface $parameterBag ) : array {
+    private function getPathEntries( array $parameters ) : array {
 
-        $parameters = \array_filter(
-            array    : $parameterBag->all(),
+
+        $paths = \array_filter(
+            array    : $parameters,
             callback : fn ( $value, $key ) => \is_string( $value ) &&
                                               ( \str_starts_with( $key, 'dir' ) ||
                                                 \str_starts_with( $key, 'path' ) ) &&
@@ -47,17 +103,17 @@ final readonly class ApplicationSettingsPass implements CompilerPassInterface
         );
 
         // Sort and normalise
-        foreach ( $parameters as $key => $value ) {
+        foreach ( $paths as $key => $value ) {
 
             // Simple sorting; unsetting 'dir' and 'path' prefixed keys, appending them after all Symfony-defined directories
             if ( \str_starts_with( $key, 'dir' ) || \str_starts_with( $key, 'path' ) ) {
-                unset( $parameters[ $key ] );
+                unset( $paths[ $key ] );
             }
 
             // Normalise each path
-            $parameters[ $key ] = normalizePath( $value );
+            $paths[ $key ] = normalizePath( $value );
         }
 
-        return $parameters;
+        return $paths;
     }
 }
