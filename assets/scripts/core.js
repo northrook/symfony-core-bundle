323266b6e845b466652369f283875bb8f1c3a9dc
Martin <hello@northrook.com>
2024-08-25 06:47:13 +0100

    mend

--- assets/scripts/core.js
+++ assets/scripts/core.js
@@ -1,51 +1,3 @@
-/**
- * @param {HTMLElement} node
- */
-function elementAnimations( node ) {
-	/** @type {ElementAnimations}  */
-	const transitions = {
-		longest    : {
-			property: null, ms: 0,
-		}, shortest: {
-			property: null, ms: 0,
-		},
-	}
-
-	if ( !( node instanceof Element ) ) {
-		console.error( 'Invalid DOM element provided.' )
-		return transitions
-	}
-
-	const style = getComputedStyle( node )
-	const delays = style.transitionDelay.split( ', ' ).map( delay => parseFloat( delay ) * 1000 )
-	const durations = style.transitionDuration.split( ', ' ).map( duration => parseFloat( duration ) * 1000 )
-	const properties = style.transitionProperty.split( ', ' )
-
-	properties.forEach( ( property, i ) => {
-		const delay = delays[i]
-		const duration = durations[i]
-		const timing = delay + duration
-
-		Object.assign( transitions, {
-			property: {
-				name: property, delay, duration, total: timing,
-			},
-		} )
-
-		if ( timing > transitions.longest.ms ) {
-			transitions.longest.property = property
-			transitions.longest.ms = timing
-		}
-
-		if ( timing < transitions.shortest.ms ) {
-			transitions.shortest.property = property
-			transitions.shortest.ms = timing
-		}
-	} )
-
-	return transitions
-}
-
 const App = {
 	events: new Map(),
 
@@ -56,9 +8,7 @@ const App = {
 	 * @return {string} App Status
 	 */
 	status: ( set = null ) => {
-		if ( set ) {
-			document.documentElement.setAttribute( 'status', set )
-		}
+		if ( set ) document.documentElement.setAttribute( 'status', set )
 		return document.documentElement.getAttribute( 'status' ) || 'unknown'
 	},
 
--- assets/scripts/elements.js
+++ assets/scripts/elements.js
@@ -145,6 +145,55 @@ function $make( html ) {
 	return ElementProxy( createElement( html ) );
 }
 
+/**
+ * @param {HTMLElement} node
+ */
+function elementAnimations( node ) {
+	
+	/** @type {ElementAnimations}  */
+	const transitions = {
+		longest    : {
+			property: null, ms: 0,
+		}, shortest: {
+			property: null, ms: 0,
+		},
+	}
+
+	if ( !( node instanceof Element ) ) {
+		console.error( 'Invalid DOM element provided.' )
+		return transitions
+	}
+
+	const style = getComputedStyle( node )
+	const delays = style.transitionDelay.split( ', ' ).map( delay => parseFloat( delay ) * 1000 )
+	const durations = style.transitionDuration.split( ', ' ).map( duration => parseFloat( duration ) * 1000 )
+	const properties = style.transitionProperty.split( ', ' )
+
+	properties.forEach( ( property, i ) => {
+		const delay = delays[i]
+		const duration = durations[i]
+		const timing = delay + duration
+
+		Object.assign( transitions, {
+			property: {
+				name: property, delay, duration, total: timing,
+			},
+		} )
+
+		if ( timing > transitions.longest.ms ) {
+			transitions.longest.property = property
+			transitions.longest.ms = timing
+		}
+
+		if ( timing < transitions.shortest.ms ) {
+			transitions.shortest.property = property
+			transitions.shortest.ms = timing
+		}
+	} )
+
+	return transitions
+}
+
 //::: Multi-node select
 // let nodes = element instanceof NodeList || Array.isArray( element )
 // 	? element
